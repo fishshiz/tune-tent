b2c735dd2b90bf2f379a59df1aa617edc81b15a0
Edan Lewis <edan@lewisarts.com>
2017-11-22 09:30:39 -0800

    reconfigures Users table for unique username and session_token.

--- tune_tent/db/migrate/20171121212426_create_users.rb
+++ tune_tent/db/migrate/20171121212426_create_users.rb
@@ -14,5 +14,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
