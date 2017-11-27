1dbed263f05453ddcf95fe2c2a829fc22715af1b
Edan Lewis <edan@lewisarts.com>
2017-11-26 17:12:19 -0800

    Adds styling for album index.

--- tune_tent/frontend/components/user_page/album_index_item.jsx
+++ tune_tent/frontend/components/user_page/album_index_item.jsx
@@ -3,12 +3,12 @@ import { Link, withRouter } from 'react-router-dom';
 
 const AlbumIndexItem = ({ album }) => {
   return (
-    <li>
-      <Link to={`/albums/${album.id}`}>
-        <img src={album.album_img_url} />
+
+      <Link className="album-bundle" to={`/albums/${album.id}`}>
+        <img className="album-img" src={album.album_img_url} />
         {album.title}
       </Link>
-    </li>
+
   );
 };
 
